8dc714937c1b24c4b7b58a1581ecc7986be0d29e
poketaro1930katsu-sys <poketaro.1930katsu@gmail.com>
2026-08-28 15:29:29 +0900

    app: bump public PWA cache to v0.4.3

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE='orz-command-v0.4.2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./public-status.json'];
+const CACHE='orz-command-v0.4.3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
+self.addEventListener('fetch',e=>{
+  const url=new URL(e.request.url);
+  if(url.pathname.endsWith('/public-status.json')){
+    e.respondWith(fetch(e.request,{cache:'no-store'}));
+    return;
+  }
+  e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)));
+});
